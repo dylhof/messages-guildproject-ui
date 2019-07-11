147a04d6882704b965c3d5d4b13abde207382f1c
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-07-11 14:56:03 -0600

    Create Message Input as control form and post new message upon supmit

---
+++ src/Components/MessageInput/MessageInput.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import uuid from 'uuid';
+import {FetchCall} from '../../utils/FetchApi';
+
+class MessageInput extends Component{
+  constructor() {
+    super();
+    this.state = {
+      message: ''
+    };
+  };
+
+  handleChange = (event) => {
+    this.setState({message: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      id: uuid(),
+      user: this.props.user,
+      message: this.state.message
+    }
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(newMessage),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    FetchCall('http://localhost:3001/chat', options);
+    this.setState({message: ''})
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          type='text' 
+          value={this.state.message} 
+          onChange={this.handleChange}
+        />
+        <input
+          type='submit'
+          value='Post Message'
+        />
+      </form>
+    )
+  };
+};
+
+export default MessageInput;
