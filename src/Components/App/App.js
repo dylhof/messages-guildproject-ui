6f10a9ab5f660abebb6d2932d0207a3d383dd82f
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-07-11 14:13:39 -0600

    Change App to a Class Component and display messageArea

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../../styles/App.css';
+import MessageArea from '../MessageArea/MessageArea';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Guild Education Messaging </h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Guild Education Messaging </h1>
+          <MessageArea messages={this.state.messages}/>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
